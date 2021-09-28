378a36179af0df27d95a0502989fae6ce3f0b93a
plh <lehoangbro@gmail.com>
2021-09-28 22:22:46 +0700

    add worker-node #2

--- main.tf
+++ main.tf
@@ -22,9 +22,9 @@ output "external_ip_master-node" {
 }
 
 output "internal_ip_worker-node" {
-  value = google_compute_instance.master-node.network_interface.0.network_ip
+  value = google_compute_instance.worker-node.network_interface.0.network_ip
 }
 
 output "external_ip_worker-node" {
-  value = google_compute_instance.master-node.network_interface.0.access_config.0.nat_ip
+  value = google_compute_instance.worker-node.network_interface.0.access_config.0.nat_ip
 }
