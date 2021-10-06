39e5294e2c747ff7c6c49801d0523d18f40fea95
plh <lehoangbro@gmail.com>
2021-10-06 10:38:46 +0000

    add install docker script

--- files/master_startup_script.sh
+++ files/master_startup_script.sh
@@ -6,10 +6,25 @@ curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 apt-get update
 
+
 # Install T.I.C.K stack
 apt-get install telegraf influxdb kapacitor chronograf
 systemctl start telegraf influxdb kapacitor chronograf
 systemctl enable telegraf influxdb kapacitor chronograf
 
-# Get metadata
-DNS_NAME=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/dns_name")
+
+# Install docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
--- files/worker_startup_script.sh
+++ files/worker_startup_script.sh
@@ -16,3 +16,19 @@ DNS_NAME=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal
 
 echo 'urls = ["http://master-node-1.plh.net:8086"]' >> /etc/telegraf/telegraf.conf
 systemctl restart telegraf
+
+# Install docker
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
