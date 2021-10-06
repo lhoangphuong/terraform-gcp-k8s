4c01901546a825e6c034762a886973f7cc6e6be2
plh <lehoangbro@gmail.com>
2021-10-06 11:12:27 +0000

    fix

--- files/master_startup_script.sh
+++ files/master_startup_script.sh
@@ -1,30 +1,32 @@
 #! /bin/bash
 
 # Add influxdata repo
-apt-get update
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-apt-get update
+apt-get -y update 
 
 
 # Install T.I.C.K stack
-apt-get install telegraf influxdb kapacitor chronograf
+apt-get -y install telegraf influxdb kapacitor chronograf
 systemctl start telegraf influxdb kapacitor chronograf
 systemctl enable telegraf influxdb kapacitor chronograf
 
 
 # Install docker
-sudo apt-get install \
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+
+apt-get -y update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+systemctl enable docker.service
+systemctl enable containerd.service
--- files/worker_startup_script.sh
+++ files/worker_startup_script.sh
@@ -1,34 +1,30 @@
 #! /bin/bash
 
 # Add influxdata repo
-apt-get update
 curl -sL https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 echo "deb https://repos.influxdata.com/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-apt-get update
+apt-get -y update
 
 # Install T.I.C.K stack
-apt-get install telegraf
+apt-get -y install telegraf
 systemctl start telegraf
 systemctl enable telegraf
 
-# Get metadata
-DNS_NAME=$(curl -f -H "Metadata-Flavor: Google" "http://metadata.google.internal/computeMetadata/v1/instance/attributes/dns_name")
-
-echo 'urls = ["http://master-node-1.plh.net:8086"]' >> /etc/telegraf/telegraf.conf
-systemctl restart telegraf
-
 # Install docker
-sudo apt-get install \
+apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
     gnupg \
     lsb-release
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
 echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+
+apt-get -y update
+apt-get -y install docker-ce docker-ce-cli containerd.io
+systemctl enable docker.service
+systemctl enable containerd.service
